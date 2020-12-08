70da840b67a953795dbf74ab7e150ff9f3dd60fa
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2020-12-07 23:13:32 -0600

    Impresión de variables en HTML

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const PrimeraApp = () => {
+
+    const saludo = {
+        nombre: 'Samuel',
+        edad: 22
+    };
+
     return (
         <>
-            <h1>Hola mundo sierra-034</h1>
+            <h1>Hola mundo</h1>
+            <pre>{ JSON.stringify(saludo, null, 3) }</pre>
             <p>Mi primera aplicación</p>
         </>
     );
