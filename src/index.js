1856c31dcd3ed986d0166a604e1199e05a2995ac
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2020-12-10 09:38:11 -0600

    Componente CounterApp

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-import PrimeraApp from './PrimeraApp';
+import CounterApp from "./CounterApp";
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp saludo="Hola, soy Goku" />, divRoot);
+ReactDOM.render(<CounterApp value={ 23 } />, divRoot);
