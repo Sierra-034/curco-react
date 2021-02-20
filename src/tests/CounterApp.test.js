2a9256e157280b9ac5ae8164384616128c47a0a5
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-02-20 15:08:51 -0600

    62: Simular eventos - Click

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -4,8 +4,14 @@ import { shallow } from 'enzyme';
 import CounterApp from '../CounterApp';
 
 describe('Pruebas sobre el componente <CounterApp />', () => {
+
+    let wrapper = shallow(<CounterApp />);
+    
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp />);
+    });
+    
     test('Debe mostrar el componente correctamente con sus valores por defecto', () => {
-        const wrapper = shallow(<CounterApp />);
         expect(wrapper).toMatchSnapshot();
     });
 
@@ -15,5 +21,17 @@ describe('Pruebas sobre el componente <CounterApp />', () => {
 
         const counterValue = parseInt(wrapper.find('h2').text());
         expect(counterValue).toBe(intitalCounterValue);
+    });
+
+    test('Debe incrementar con el botón +1', () => {
+        wrapper.find('button').at(0).simulate('click'); // Se puede mandar información del evento con un segundo argumento de tipo objeto
+        const counterText = wrapper.find('h2').text();
+        expect(counterText).toBe('11');
+    });
+
+    test('Debe decrementar con el botón -1', () => {
+        wrapper.find('button').at(2).simulate('click'); // Se puede mandar información del evento con un segundo argumento de tipo objeto
+        const counterText = wrapper.find('h2').text();
+        expect(counterText).toBe('9');
     });
 });
